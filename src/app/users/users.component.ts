180c6e8e31fff0c0875e28675116f10ec14b70a4
sakshi <sakshikataria59@yahoo.com>
2018-08-10 00:42:01 +0530

    first commit

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '../../../node_modules/@angular/router';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+  users = [
+    {
+      id: 1,
+      name: 'Max'
+    },
+    {
+      id: 2,
+      name: 'Anna'
+    },
+    {
+      id: 3,
+      name: 'Chris'
+    }
+  ];
+
+  onLoadServers() {
+    //complex calculation
+    this.router.navigate(['/servers']);
+  }
+}
